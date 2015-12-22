4325cace18a27193f2b699ca8b4620f385746694
sherlockHK <576717731@qq.com>
2015-12-22 22:23:06 +0800

    calculate spare round total score

--- src/Round.java
+++ src/Round.java
@@ -1,13 +1,21 @@
 public class Round {
     private int firstBall;
     private int secondBall;
+    private Round nextRound;
 
     public Round(int firstBall, int secondBall) {
         this.firstBall = firstBall;
         this.secondBall = secondBall;
     }
 
+    public void setNextRound(Round nextRound) {
+        this.nextRound = nextRound;
+    }
+
     public int calculateTotalScore() {
+        if (firstBall != 10 && firstBall+secondBall == 10 && nextRound != null){
+            return firstBall+secondBall+nextRound.firstBall;
+        }
         return firstBall+secondBall;
     }
 
--- test/RoundTest.java
+++ test/RoundTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class RoundTest {
@@ -11,5 +12,11 @@ public class RoundTest {
         assertThat(totalScore,is(3));
     }
 
-
+    @Test
+    public void shouldCalculateScoreWithBonus() {
+        Round round1 = new Round(1,9);
+        Round round2 = new Round(4,6);
+        round1.setNextRound(round2);
+        assertEquals(14,round1.calculateTotalScore());
+    }
 }
